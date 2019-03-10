7f1499ffcbcef640448a74944017a2392cc3028a
Damien Arondel <damien.arondel@icloud.com>
2019-03-10 23:30:32 +0100

    feat(music): update environments to store last.fm API URL (#4)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
   authUrl: 'INSERT_AUTH_API',
+  lastFmApi: {
+    apiKey: 'INSERT_LAST_FM_API_KEY',
+    urls: {
+      topTracks: 'http://ws.audioscrobbler.com/2.0/?method=chart.gettoptracks&format=json&api_key='
+    }
+  },
   production: false
 };
