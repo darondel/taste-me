0daf60d995edb1309e27f9517c51a4179e9c7314
Damien Arondel <damien.arondel@icloud.com>
2019-03-10 18:11:40 +0100

    feat(menu): add a side menu plus a toggle button on home page (#3)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,54 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-import { Platform } from '@ionic/angular';
+import { MenuController, Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import { AuthService } from './auth/services/auth.service';
+
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html'
+  templateUrl: 'app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+
+  pages = [
+    {title: 'Dashboard', enabled: false, url: '/dashboard', icon: 'analytics'},
+    {title: 'Player', enabled: false, url: '/player', icon: 'musical-note'},
+    {title: 'Musics', enabled: false, url: '/musics', icon: 'musical-notes'},
+    {title: 'Chat', enabled: false, url: '/chat', icon: 'chatboxes'}
+  ];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
-  ) {
+    private statusBar: StatusBar,
+    private authService: AuthService,
+    private router: Router,
+    private menuController: MenuController) {
     this.initializeApp();
   }
 
+  /**
+   * Initializes the application.
+   */
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
+
+  /**
+   * Handles logout demand.
+   * Redirects to login page after it.
+   */
+  onLogout() {
+    this.authService.logout();
+    this.menuController.close();
+    this.router.navigate(['/login']);
+  }
+
 }
